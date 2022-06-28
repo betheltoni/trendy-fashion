298a567ef593d7c75b64b59c7e01c8fe82aae86a
bethel enunwa <enunwabethel@gmail.com>
2022-06-28 14:29:16 +0100

    Feat: Implement Header Html
    created the NavBar component and Implemented
    the structure of the Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.scss';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Hero from './Components/Hero/Hero';
 
 function App() {
   return (
     <div className='App'>
-      
+      <Header/>
+      <Hero/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AboutUs from './AboutUs/AboutUs'
+import "./Hero.scss"
+
+const Hero = () => {
+  return (
+    <div>
+      <AboutUs/>
+    </div>
+  )
+}
+
+export default Hero
