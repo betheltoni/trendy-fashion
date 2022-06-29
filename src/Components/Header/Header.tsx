dcebc8ea2e095eab7b55c9ea2dcd948e66569d09
bethel enunwa <enunwabethel@gmail.com>
2022-06-29 14:03:26 +0100

    Style Header Component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,17 +4,16 @@ import NavBar from './NavBar/NavBar'
 
 const Header = () => {
   return (
-    <div>
+    <section className='Header'>
       <NavBar/>
       <h1>
         Trendy <br /> Fashion
       </h1>
-      <svg width="90" height="90" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg width="70" height="70" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M7.5 45C7.5 65.7107 24.2893 82.5 45 82.5C65.7107 82.5 82.5 65.7107 82.5 45C82.5 24.2893 65.7107 7.5 45 7.5C24.2893 7.5 7.5 24.2893 7.5 45Z" stroke="white" stroke-width="2"/>
         <path d="M60 39.375L45 54.375L30 39.375" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
-
-    </div>
+    </section>
   )
 }
 
